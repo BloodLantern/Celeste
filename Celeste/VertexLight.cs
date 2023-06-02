26ec96219f22a5c2a5cbcf3a6e4ffc732a08a442
BloodLantern <maxime.gri.berlioz@gmail.com>
2023-06-02 10:17:07 +0200

    Removed DotPeek auto-generated comments

--- Celeste/VertexLight.cs
+++ Celeste/VertexLight.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Celeste.VertexLight
-// Assembly: Celeste, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: FAF6CA25-5C06-43EB-A08F-9CCF291FE6A3
-// Assembly location: C:\Program Files (x86)\Steam\steamapps\common\Celeste\orig\Celeste.exe
-
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Monocle;
 using System;
 
--- FMOD/DEBUG_CALLBACK.cs
+++ FMOD/DEBUG_CALLBACK.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: FMOD.DEBUG_CALLBACK
-// Assembly: Celeste, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: FAF6CA25-5C06-43EB-A08F-9CCF291FE6A3
-// Assembly location: C:\Program Files (x86)\Steam\steamapps\common\Celeste\orig\Celeste.exe
-
-namespace FMOD
+﻿namespace FMOD
 {
     public delegate RESULT DEBUG_CALLBACK(
         DEBUG_FLAGS flags,
